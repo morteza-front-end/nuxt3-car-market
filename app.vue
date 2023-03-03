7713fbaa7645b061b3e629d579cb285e20adbaa4
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-03 16:20:13 +0330

    handle home page

--- app.vue
+++ app.vue
@@ -12,8 +12,28 @@
     <!-- HOME HERO   -->
     <div class="relative h-screen w-full bg-no-repeat bg-cover
     bg-bottom bg-[url(/images/car-ferrari.jpg)]">
-
+      <div
+          class="absolute w-full h-full z-10 flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl text-white font-serif mb-10">
+            Find your drive</h1>
+          <!--SEARCH Bar-->
+          <div
+              class="font-serif w-[1000px] text-2xl rounded-full bg-white flex
+               justify-between items-center overflow-hidden drop-shadow-2xl mx-auto">
+            <input type="text" class="px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="search by city..." />
+            <button class="bg-sky-500 px-10 py-3 text-white">
+              Search
+            </button>
+          </div>
+          <!--SEARCH Bar-->
+        </div>
+      </div>
+      <!--  Black Page      -->
+      <div
+          class="absolute w-full h-full bg-black opacity-50"></div>
     </div>
     <!-- HOME HERO   -->
   </div>
 </template>
+
