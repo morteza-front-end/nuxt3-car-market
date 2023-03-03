122648b6537245e476f562bf317a4329e2574cc9
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-03 22:13:37 +0330

    create custom error page

---
+++ error.vue
@@ -0,0 +1,20 @@
+<script setup>
+useHead({
+  title: 'Error Page'
+})
+
+function handleError() {
+  navigateTo('/')
+}
+</script>
+<template>
+  <div
+      class="h-screen w-full flex flex-col items-center justify-center">
+    <h1 class="text-9xl">404</h1>
+    <p class="text-5xl my-4">Page not found!</p>
+    <button
+        class="border border-sky-500 bg-gray-400 rounded py-3 px-4 mt-4 text-white"
+        @click="handleError">Go Back
+    </button>
+  </div>
+</template>
