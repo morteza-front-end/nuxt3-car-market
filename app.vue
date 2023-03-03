1d0ef8ac6c3e90beb18bf4a39fe78ee7bd118bca
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-03 15:50:40 +0330

    add image car for background

--- app.vue
+++ app.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <!-- NAVBAR    -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4
+    <header class="sticky top-0 z-50 flex
+     justify-between items-center
+     space-x-1 border-b bg-white p-4
       shadow-lg">
-      <a class="text-3xl font-mono" href="#">Car Market</a>
+      <a class="text-3xl font-mono" href="#">Car
+        Market</a>
     </header>
     <!-- NAVBAR    -->
+    <!-- HOME HERO   -->
+    <div class="relative h-screen w-full bg-no-repeat bg-cover
+    bg-bottom bg-[url(/images/car-ferrari.jpg)]">
+
+    </div>
+    <!-- HOME HERO   -->
   </div>
 </template>
