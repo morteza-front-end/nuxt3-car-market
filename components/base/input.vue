92db296a07573bda83fcd78f81f131924880854c
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-03 20:05:05 +0330

    handle v-model

--- components/base/input.vue
+++ components/base/input.vue
@@ -3,8 +3,13 @@
       type="text"
       class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
       placeholder="Search by city..."
+      @input="update($event)"
   />
 </template>
 <script setup>
-
+const props = defineProps(['modelValue'])
+const emit= defineEmits(['update:modelValue'])
+const update =(event)=>{
+  emit('update:modelValue',event.target.value)
+}
 </script>
