9d4b68904191af1a3dd00174a67ae3526a3230cd
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-05 23:09:33 +0330

    handle error client side

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -10,15 +10,25 @@ definePageMeta({
 
 </script>
 <template>
-    <div class="mt-32 flex">
-      <!-- CAR SIDE BAR -->
-      <base-sideBar/>
-      <!-- CAR SIDE BAR -->
+    <div class="mt-32 flex justify-center">
+      <NuxtErrorBoundary>
+        <!-- CAR SIDE BAR -->
+        <base-sideBar/>
+        <!-- CAR SIDE BAR -->
 
-      <!-- CAR CARDS -->
-      <div class="w-full">
-        <nuxt-page/>
-      </div>
-      <!-- CAR CARDS -->
+        <!-- CAR CARDS -->
+        <div class="w-full">
+          <nuxt-page/>
+        </div>
+        <!-- CAR CARDS -->
+        <template #error="{error}">
+          <div class="w-1/2 flex flex-col items-center justify-center">
+            <h1 class="text-red-600 text-5xl mb-4">Some thing is wrong!</h1>
+            <code>{{error}}</code>
+            <button class="bg-sky-500 rounded px-8 py-4 mt-4" @click="error.value=null">Go Back</button>
+          </div>
+        </template>
+      </NuxtErrorBoundary>
+
     </div>
 </template>
