d2a0b91a5ef62d1fba3f9c32f7d0dc354de66d13
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2023-03-05 19:49:36 +0330

    init modal

---
+++ components/base/modal.vue
@@ -0,0 +1,17 @@
+<script setup>
+const emit = defineEmits(['close'])
+function close(){
+  emit('close')
+}
+</script>
+<template>
+  <div
+      class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
+    <input type="text"
+           class="border p-1 rounded"/>
+    <button @click="close"
+        class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+      Apply
+    </button>
+  </div>
+</template>
